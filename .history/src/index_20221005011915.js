71c1a4f3c1ac3805c2e4a958ae79be96a06c6137
Songeun <songsilver.lee@gmail.com>
2022-10-09 22:55:49 +0900

    init project

--- .history/src/index_20221005011915.js
+++ .history/src/index_20221005011915.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./app";
+import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -9,3 +9,5 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
+reportWebVitals();
